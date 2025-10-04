cc22f1c641999c5e467b4fb3647d0b37e8be9eda
Furkan Nabi Sumji <furkannabisumji@gmail.com>
2025-10-04 18:37:28 +0530

    Update MemedFactory and MemedTokenSale contract references to remove '_test' suffix. This change aligns the module definitions with the production contracts, enhancing clarity and consistency in the codebase.

--- ignition/modules/Factory.ts
+++ ignition/modules/Factory.ts
@@ -5,6 +5,6 @@ export default buildModule("FactoryModule", (m) => {
   const memedBattle = m.getParameter("memedBattle");
   const memedEngageToEarn = m.getParameter("memedEngageToEarn");
   const uniswapV2Router = m.getParameter("uniswapV2Router");
-  const factory = m.contract("MemedFactory_test", [memedTokenSale, memedBattle, memedEngageToEarn, uniswapV2Router]);
+  const factory = m.contract("MemedFactory", [memedTokenSale, memedBattle, memedEngageToEarn, uniswapV2Router]);
   return { factory };
 });
