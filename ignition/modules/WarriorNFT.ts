b0bfaf72546497b531202fafb6be16f105fa7517
Furkan Nabi Sumji <furkannabisumji@gmail.com>
2025-10-08 17:19:54 +0530

    Remove Token and WarriorNFT deployment modules from package.json and delete their corresponding TypeScript files to streamline the project structure and eliminate unused code.

--- ignition/modules/WarriorNFT.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("WarriorNFTModule", (m) => {
-  const memedToken = m.getParameter("token");
-  const memedBattle = m.getParameter("battle");
-  const factory = m.getParameter("factory");
-  const warriorNFT = m.contract("MemedWarriorNFT", [memedToken, memedBattle, factory]);
-  return { warriorNFT };
-});
