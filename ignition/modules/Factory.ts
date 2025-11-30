d04f7051999e2bd1e51df069fe0948913e6f0ae3
Furqaan Nabi <furkannabisumji@gmail.com>
2025-11-30 14:21:40 +0530

    Remove test contracts for MemedFactory and MemedTokenSale, streamlining the codebase by eliminating unused files. Update Factory and TokenSale modules to reference the main contracts instead of their test counterparts, ensuring consistency in contract deployment.

--- ignition/modules/Factory.ts
+++ ignition/modules/Factory.ts
@@ -6,6 +6,6 @@ export default buildModule("FactoryModule", (m) => {
   const memedEngageToEarn = m.getParameter("memedEngageToEarn");
   const uniswapV3PositionManager = m.getParameter("uniswapV3PositionManager");
   const uniswapV3SwapRouter = m.getParameter("uniswapV3SwapRouter");
-  const factory = m.contract("MemedFactory_test", [memedTokenSale, memedBattle, memedEngageToEarn, uniswapV3PositionManager, uniswapV3SwapRouter]);
+  const factory = m.contract("MemedFactory", [memedTokenSale, memedBattle, memedEngageToEarn, uniswapV3PositionManager, uniswapV3SwapRouter]);
   return { factory };
 });
